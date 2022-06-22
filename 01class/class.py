066f67710422403a1bd9c5b5109d71639a2cc7dc
MOHD SAQIB ANSARI <106581457+mohdsaqibansari@users.noreply.github.com>
2022-06-22 18:52:47 +0530

    Add files via upload

---
+++ 01class/class.py
@@ -0,0 +1,23 @@
+# Class is a Blueprint.
+# Every Object which is present belongs to a class.
+a=2
+print(type(a)) # You will see the class int
+# It means that every variable is an object in the Python.
+
+# List is a class which has functions in it.
+l=[1,2,3,4]
+l.append(5)
+print(l)
+# A data-type is a class and variable that we make of that class 
+# is an Object of that class.
+# EX: List is a class and l=[1,2] , here, l is an object of class List.
+
+
+# Class has two things : Data(Property) and Functions(Behavior)
+# Example of Structure of Class
+# class Car:
+#   color="black" #data
+#   model="2022" #data
+
+#   def calc_avg_speed(dist,time):
+#     some code
